2f3232536436af2b9ec586569e1c2279c4ba16a7
James Hanlon <mail@jameswhanlon.com>
2024-01-20 21:24:41 +0000

    Add pre-commit hooks and update README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # finances
+
+Steps to get going:
+
+```
+$ python -m venv env
+$ source env/bin/activate
+$ pip install -r requirements.txt
+```
+
+Access data held in Google Sheets using gspread Python package.
+
+Setup API access using these instructions:
+
+https://docs.gspread.org/en/latest/oauth2.html#enable-api-access-for-a-project
--- test.py
+++ test.py
@@ -2,7 +2,16 @@ import gspread
 
 gc = gspread.oauth()
 
-sheet = gc.open("Finances-2024")
+sheet_names = [
+        "Spending-2019",
+        "Spending-2020",
+        "Spending-2021",
+        "Spending-2022",
+        "Spending-2023",
+        "Spending-2024",
+        ]
+
+sheet = gc.open(sheet_names[-1])
 worksheet = sheet.get_worksheet(0)
 
 values = worksheet.get_all_values()
